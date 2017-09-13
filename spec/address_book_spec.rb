e3b6dfd0b922aace309448b961987b5fe811de4c
Eric Albertus <eralchemist@gmail.com>
2017-09-12 21:55:07 -0700

    assignment-6-read-csvs complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -111,7 +111,35 @@ let(:book) { AddressBook.new }
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
    end
- 
+ describe "#import from entries_2.csv" do
+	it "imports the correct number of entries" do
 
-	
+	book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+ 
+       
+       expect(book_size).to eq 3
+     end
+	it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the first entry
+       entry_one = book.entries[0]
+ 
+     
+       check_entry(entry_one, "Billy", "557-955-4854", "billy@hotmail.com")
+     end
+  it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+        check_entry(entry_two, "Bobby", "550-575-5415", "bobby@hotmail.com")
+     end
+ 
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the third entry
+		entry_three = book.entries[2]
+       check_entry(entry_three, "Joey", "555-595-3060", "joey@hotmail.com")
+     end
+	end
  end
